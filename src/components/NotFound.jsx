a4530609624c4474bd98ff518b050f256d38d890
shubhamchasing <shubhamchasing@gmail.com>
2024-12-12 20:48:53 +0530

    fix: make notfound page div a flex

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,7 +3,15 @@ import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div style={{ textAlign: "center", marginTop: "150px" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "1em",
+      }}
+    >
       <h1>404</h1>
       <p>The page you're looking for doesn't exist.</p>
       <Link to="/">Go back to Home</Link>
